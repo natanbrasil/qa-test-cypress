1b16ed8b5248ddc29c3255d1dbf10c0c9e2fda50
Natan Brasil <natanvicenti10@gmail.com>
2025-04-14 13:45:56 -0300

    put const selectorlist for use in tests

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,18 +1,27 @@
 describe ('Orange HRM Tests', () => {
+
+    const selectorList = {
+        usernameField: "input[placeholder='Username']",
+        passwordField: "input[placeholder='Password']",
+        loginButton: "button[type='submit']",
+        sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+        alertMessage: "div[role='alert']"
+    }
+
     it('Login-Sucess', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get("input[placeholder='Username']").type('Admin')
-        cy.get("input[placeholder='Password']").type('admin123')
-        cy.get("button[type='submit']").click()
+        cy.get(selectorList.usernameField).type('Admin')
+        cy.get(selectorList.passwordField).type('admin123')
+        cy.get(selectorList.loginButton).click()
         cy.url('pathname').should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index') 
-        cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+        cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
     })
     it('Login-Fail', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get("input[placeholder='Username']").type('Admin1')
-        cy.get("input[placeholder='Password']").type('admin1234')
-        cy.get('.oxd-button').click()
-        cy.get("div[role='alert']").should('be.visible')
+        cy.get(selectorList.usernameField).type('Admin1')
+        cy.get(selectorList.passwordField).type('admin1234')
+        cy.get(selectorList.loginButton).click()
+        cy.get(selectorList.alertMessage).should('be.visible')
         //caso queira pular esse teste pode user o it.skip
     })
 })
