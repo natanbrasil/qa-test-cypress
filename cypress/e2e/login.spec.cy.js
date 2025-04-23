6c83c4c29cfcf76b7bfaa30ce1089444bebc7985
Natan Brasil <natanvicenti10@gmail.com>
2025-04-23 19:01:29 -0300

    create userData.json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,3 +1,5 @@
+import userdata from '../fixtures/users/userData.json'
+
 describe ('Orange HRM Tests', () => {
 
     const selectorList = {
@@ -10,16 +12,16 @@ describe ('Orange HRM Tests', () => {
 
     it('Login-Sucess', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get(selectorList.usernameField).type('Admin')
-        cy.get(selectorList.passwordField).type('admin123')
+        cy.get(selectorList.usernameField).type(userdata.userSucess.username)
+        cy.get(selectorList.passwordField).type(userdata.userSucess.password)
         cy.get(selectorList.loginButton).click()
         cy.url('pathname').should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index') 
         cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
     })
     it('Login-Fail', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get(selectorList.usernameField).type('Admin1')
-        cy.get(selectorList.passwordField).type('admin1234')
+        cy.get(selectorList.usernameField).type(userdata.Userfail.username)
+        cy.get(selectorList.passwordField).type(userdata.Userfail.password)
         cy.get(selectorList.loginButton).click()
         cy.get(selectorList.alertMessage).should('be.visible')
         //caso queira pular esse teste pode user o it.skip
