5163387a48ee4ea13c0b51674cb8df1c0497503f
Natan Brasil <natanvicenti10@gmail.com>
2025-04-24 18:10:29 -0300

    add file user.spec and edit user info

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,16 +7,30 @@ describe ('Orange HRM Tests', () => {
         passwordField: "input[placeholder='Password']",
         loginButton: "button[type='submit']",
         sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-        alertMessage: "div[role='alert']"
+        dashboardGrid: ".orangehrm-dashboard-grid",
+        myInfoButton: ":nth-child(6) > .oxd-main-menu-item",
+        alertMessage: "div[role='alert']",
+        firstNameField: "input[placeholder='First Name']",
+        lastNameField: "input[placeholder='Last Name']",
+        driverLicenseField: ":nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
+        dateField: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
+        dateCloseButton: ".oxd-form > :nth-child(3) > :nth-child(2)"
+
     }
 
-    it('Login-Sucess', () => {
+    it.only('User Info Update - Sucess', () => {
         cy.visit('/auth/login')
         cy.get(selectorList.usernameField).type(userdata.userSucess.username)
         cy.get(selectorList.passwordField).type(userdata.userSucess.password)
         cy.get(selectorList.loginButton).click()
         cy.url('pathname').should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index') 
-        cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+        cy.get(selectorList.dashboardGrid)
+        cy.get(selectorList.myInfoButton).click()
+        cy.get(selectorList.firstNameField).clear().type('Natan')
+        cy.get(selectorList.lastNameField).clear().type('Lima')
+        cy.get(selectorList.driverLicenseField).clear().type('123456789')
+        cy.get(selectorList.dateField).clear().type('2025-10-01')
+        cy.get(selectorList.dateCloseButton).click()
     })
     it('Login-Fail', () => {
         cy.visit('/auth/login')
